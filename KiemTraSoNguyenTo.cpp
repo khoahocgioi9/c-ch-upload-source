2dc44ac74d991a9bdef9c6085ac943c69ec94f4d
khoahocgioi9 <khoahocgioi9@gmail.com>
2020-11-22 16:08:41 +0700

    first commit

---
+++ KiemTraSoNguyenTo.cpp
@@ -0,0 +1,22 @@
+
+int main() {
+	int x;
+	cout << " nhap x: ";
+	cin >> x;
+	if (x < 2){
+		cout << x << " ko la so nguyen to";
+	}
+	int tong = 0;
+	if (int i = 2; i <= sqrt(x); i++){
+		if (n%i == 0){
+			count++;
+		}
+
+	}
+	if (count = 0){
+		cout << x << " la so nguyen to";
+	}
+	else{
+		cout << x << " ko la so nguyen to";
+	}
+}
